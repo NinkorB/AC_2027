644759381f58f853998a79d24b195d356ffb4290
Ninkor Chandra Barman <ninkor@Ninkors-MacBook-Air.local>
2025-01-08 23:39:50 +0530

    POTD_4 QUESTION NO 2

---
+++ POTD_4/pangram.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+    cin >> n;
+    cin >> s;
+
+    int letters[26] = {0};
+    for (char c : s) {
+        if (c >= 'a' && c <= 'z') {
+            letters[c - 'a'] = 1;
+        } else if (c >= 'A' && c <= 'Z') {
+            letters[c - 'A'] = 1;
+        }
+    }
+
+    int count = 0;
+    for (int i = 0; i < 26; i++) {
+        count += letters[i];
+    }
+
+    if (count == 26) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
